92d78610a40ac3cbc4d5d089fbfdef023ee41317
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-08-14 14:29:39 +0100

    Add flake.nix/lock, add nix CI

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description =
+    ''
+    Resize, list/get/put files and shell into raw disk images
+    '';
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        disk-img-tool = pkgs.python3Packages.buildPythonApplication {
+          pname = "disk-img-tool";
+          version = "0.1.0";
+          src = self;
+        };
+      in
+      rec {
+        packages.default = disk-img-tool;
+        apps.default = {
+          disk-img-tool = {
+            type = "app";
+            program = "${disk-img-tool}/bin/disk-img-tool";
+          };
+        };
+      }
+    );
+}
